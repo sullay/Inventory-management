6009ccbd31b31102db72e2ee6c90e290a57633d1
sullay <1181518458@qq.com>
2018-01-17 20:16:43 +0800

    sullay

---
+++ IM-web/src/utils/axios.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+let base = ''
+export const postRequest = (url, params) => {
+  return axios({
+    method: 'post',
+    url: `${base}${url}`,
+    data: params
+  })
+}
+export const putRequest = (url, params) => {
+  return axios({
+    method: 'put',
+    url: `${base}${url}`,
+    data: params
+  })
+}
+export const deleteRequest = (url, params) => {
+  return axios({
+    method: 'delete',
+    url: `${base}${url}`,
+    data: params
+  })
+}
+export const getRequest = (url, params) => {
+  return axios({
+    method: 'get',
+    data: params,
+    url: `${base}${url}`
+  })
+}
---
+++ IM-web/src/views/Login.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-form :model="loginForm" :rules="rules" class="login-container" label-position="left"
+           label-width="0px" v-loading="loading" ref="loginForm">
+    <h3 class="login_title">系统登录</h3>
+    <el-form-item prop="username">
+      <el-input type="text" v-model="loginForm.username" placeholder="账号"></el-input>
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input type="password" v-model="loginForm.password" placeholder="密码"></el-input>
+    </el-form-item>
+    <el-form-item style="width: 100%">
+      <el-button type="primary" @click="submitClick" style="width: 100%">登录</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+<script>
+  export default{
+    data () {
+      return {
+        rules: {
+          username: [{required: true, message: '请输入用户名', trigger: 'blur'}],
+          password: [{required: true, message: '请输入密码', trigger: 'blur'}]
+        },
+        loginForm: {
+          username: 'aaa',
+          password: 'aaa'
+        },
+        loading: false
+      }
+    },
+    methods: {
+      submitClick () {
+        this.$refs.loginForm.validate((valid) => {
+          if (valid) {
+            alert('submit!')
+          } else {
+            console.log('error submit!!')
+            return false
+          }
+        })
+      }
+    }
+  }
+</script>
+<style>
+  .login-container {
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 180px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+  }
+
+  .login_title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+  }
+
+  .login_remember {
+    margin: 0px 0px 35px 0px;
+    text-align: left;
+  }
+</style>
