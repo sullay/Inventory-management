35a0728e7d2468cf7053e51c014a66068f21fdf6
sullay <1181518458@qq.com>
2018-01-19 00:22:51 +0800

    sullay

---
+++ IM-web/src/components/sideBar.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+<el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+  <el-radio-button :label="false">展开</el-radio-button>
+  <el-radio-button :label="true">收起</el-radio-button>
+</el-radio-group>
+<el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+
+    <el-submenu index="1-4">
+       <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+      <el-menu-item index="1-4-1">选项1</el-menu-item>
+    </el-submenu>
+  <el-menu-item index="2">
+    <i class="el-icon-menu"></i>
+    <span slot="title">导航二</span>z
+  </el-menu-item>
+  <el-menu-item index="3">
+    <i class="el-icon-setting"></i>
+    <span slot="title">导航三</span>
+  </el-menu-item>
+</el-menu>
+  </div>
+</template>
+
+<style>
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+</style>
+
+<script>
+  export default {
+    data () {
+      return {
+        isCollapse: true
+      }
+    },
+    methods: {
+      handleOpen (key, keyPath) {
+        console.log(key, keyPath)
+      },
+      handleClose (key, keyPath) {
+        console.log(key, keyPath)
+      }
+    }
+  }
+</script>
--- IM-web/src/router/index.js
+++ IM-web/src/router/index.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: index
-    }
+    // {
+    //   path: '/',
+    //   name: 'index',
+    //   component: index
+    // }
   ]
 })
