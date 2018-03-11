81773991c566e18fdff76f1f17c715255a7b9a7e
sullay <1181518458@qq.com>
2018-03-11 21:08:19 +0800

    sullay

--- IM-api/src/main/java/com/sullay/web/WarehouseController.java
+++ IM-api/src/main/java/com/sullay/web/WarehouseController.java
@@ -2,7 +2,6 @@ package com.sullay.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
